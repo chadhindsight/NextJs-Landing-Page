3457f05fefe5ff55f24886df4d96af78e1f4d161
chad hinds <chadrickj8@gmail.com>
2022-12-31 12:01:13 -0500

    Build the Navbar and Jumbotron

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,14 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import { NextUIProvider, createTheme } from '@nextui-org/react';
 
+const darkTheme = createTheme({ type: "dark" });
+
+// Exporting this way is better for tree shaking
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <NextUIProvider theme={darkTheme}>
+      <Component {...pageProps} />
+    </NextUIProvider>
+  )
 }
